0ddd6913cbef8800ca1aac64aee4f52b4ed555e2
Kazuma Sakamoto <55006769+JokerJack13@users.noreply.github.com>
2019-09-16 16:36:48 +0900

    Create askings_conrtollers.rb

---
+++ askings_conrtollers.rb
@@ -0,0 +1,27 @@
+class AskingsController < ApplicationController
+  def new
+    @ask = Ask.new
+  end
+
+  def check
+    @ask = Ask.new(ask_params)
+    if @ask.save
+      render "check"
+    else
+      render :new
+    end
+  end
+
+  def thanks
+     @ask = Ask.new(ask_params)
+    AskMailer.received_email(@ask).deliver_now
+  end
+  
+  
+  private
+
+    def ask_params
+      params.require(:ask).permit(:name, :email, :relationship, :content)
+    end
+  
+end
